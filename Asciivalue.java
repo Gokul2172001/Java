423ab3c07370a98a5ca47bb87640e44bc09ee6d0
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-01-18 23:24:12 +0530

    Add files via upload
    Ascii value

---
+++ Asciivalue.java
@@ -0,0 +1,12 @@
+//Java program for ASCII value of character.
+import java.util.Scanner;
+class Ascii {
+	public static void main(String args[]) {
+		char ch='P';
+		int asciicode=ch;
+		int asciivalue=(int)ch;
+		System.out.println("The ascii value of "+ch+" is "+asciivalue);
+	}
+}
+
+		
