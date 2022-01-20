262134c944adf9de5a4fcf569437395a81b54cdb
Gokul2172001 <76160646+Gokul2172001@users.noreply.github.com>
2022-01-20 22:37:00 +0530

    Add files via upload
    commited.

---
+++ areaofcircle.java
@@ -0,0 +1,16 @@
+//Java program for calculate area of circle
+import java.util.Scanner;
+import java.lang.Math;
+class areacircle
+{
+	private static Scanner sc;
+	public static void main(String args[])
+	{
+		double radius,area,pi=3.14;
+		sc=new Scanner(System.in);
+		System.out.println("Please Enter the radius o circle...");
+		radius=sc.nextDouble();
+		area=pi*radius*radius;
+		System.out.println("The area o the circle is..... "+area);
+	}
+}
